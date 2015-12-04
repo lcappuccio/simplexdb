799a6267a5091012f9e7d941ac7624bb265b3f24
Leonardo Cappuccio <leo@systemexception.org>
2015-12-04 23:30:32 +0100

    init application

---
+++ src/main/java/org/systemexception/simplexdb/Application.java
@@ -0,0 +1,15 @@
+package org.systemexception.simplexdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * @author leo
+ * @date 04/12/15 23:27
+ */
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
