a2de2678334eecee9774d453925e27cc987055bb
Leonardo Cappuccio <leo@systemexception.org>
2015-12-05 23:26:54 +0100

    add test properties

--- src/test/java/org/systemexception/simplexdb/test/SimplexDbControllerTest.java
+++ src/test/java/org/systemexception/simplexdb/test/SimplexDbControllerTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,6 +27,7 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {Application.class})
+@TestPropertySource(locations="classpath:application.properties")
 public class SimplexDbControllerTest {
 
 	private DatabaseService databaseService;
