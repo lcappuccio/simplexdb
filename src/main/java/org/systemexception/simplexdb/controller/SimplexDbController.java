2af1f75a09ce058e5240d0aa46fa908b823b6ec8
Leonardo Cappuccio <leo@systemexception.org>
2015-12-05 21:17:38 +0100

    implement findbyid

--- src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
+++ src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
@@ -4,17 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.systemexception.simplexdb.database.DatabaseService;
 import org.systemexception.simplexdb.domain.Data;
 import org.systemexception.simplexdb.domain.DataId;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author leo
@@ -49,5 +49,24 @@ public class SimplexDbController {
 	List<DataId> findAll() {
 		logger.info("Find all ids");
 		return databaseService.findAll();
+	}
+
+	@RequestMapping(value = "{id:.+}")
+	HttpStatus findById(@PathVariable("id") String id) throws IOException {
+		logger.info("Find " + id);
+		DataId dataId = new DataId(id);
+		Optional<Data> data = databaseService.findById(dataId);
+
+		if (data.equals(Optional.empty())) {
+			return HttpStatus.NOT_FOUND;
+		} else {
+			File dataFile = new File(dataId.getDataId());
+			try (FileOutputStream fos = new FileOutputStream(dataFile)) {
+				fos.write(data.get().getDataData());
+			} catch (Exception e) {
+				logger.error(e.getMessage());
+			}
+			return HttpStatus.FOUND;
+		}
 	}
 }
