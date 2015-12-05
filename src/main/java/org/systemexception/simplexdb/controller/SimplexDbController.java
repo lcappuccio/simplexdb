25465e5bc51f510f6383d59d9b23c3f695671414
Leonardo Cappuccio <leo@systemexception.org>
2015-12-05 17:56:13 +0100

    wip controller

--- src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
+++ src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
@@ -1,12 +1,57 @@
 package org.systemexception.simplexdb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.systemexception.simplexdb.database.DatabaseService;
+import org.systemexception.simplexdb.domain.Data;
+import org.systemexception.simplexdb.domain.DataId;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 /**
  * @author leo
  * @date 05/12/15 00:55
  */
 @RestController
+@RequestMapping(value = "/simplexdb")
 public class SimplexDbController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final DatabaseService databaseService;
+
+	@Autowired
+	public SimplexDbController(final DatabaseService databaseService) {
+		this.databaseService = databaseService;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	HttpStatus save(final File dataFile) throws IOException {
+		DataId dataId = new DataId(dataFile.getName());
+		Path dataPath = dataFile.toPath();
+		Data data = new Data(dataId, Files.readAllBytes(dataPath));
+		logger.info("Save " + dataId);
+		boolean saved = databaseService.save(data);
+		if (saved) {
+			return HttpStatus.CREATED;
+		} else {
+			return HttpStatus.BAD_REQUEST;
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	List<DataId> findAll() {
+		logger.info("Find all ids");
+		return databaseService.findAll();
+	}
 }
