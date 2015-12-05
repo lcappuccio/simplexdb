d3a3cda3ae7b627efd1e75ce262491e21f3eafd7
Leonardo Cappuccio <leo@systemexception.org>
2015-12-05 04:12:16 +0100

    added loggers

--- src/main/java/org/systemexception/simplexdb/database/DatabaseService.java
+++ src/main/java/org/systemexception/simplexdb/database/DatabaseService.java
@@ -3,6 +3,8 @@ package org.systemexception.simplexdb.database;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.mapdb.HTreeMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.systemexception.simplexdb.domain.Data;
 import org.systemexception.simplexdb.domain.DataId;
@@ -19,50 +21,63 @@ import java.util.Optional;
 @Component
 public class DatabaseService implements DatabaseApi {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private final DB database;
 	private HTreeMap<DataId, byte[]> databaseMap;
 
 	public DatabaseService(final String databaseName) {
+		logger.info("Creating database " + databaseName);
 		database = DBMaker.fileDB(new File(databaseName)).transactionDisable().closeOnJvmShutdown().make();
 		databaseMap = database.hashMap("dataCollection");
 	}
 
 	@Override
 	public boolean save(Data data) {
+		logger.info("Save " + data.getDataId().getDataId());
 		if (databaseMap.containsKey(data.getDataId())) {
+			logger.info(data.getDataId().getDataId() + " already exists");
 			return false;
 		} else {
 			databaseMap.put(data.getDataId(), data.getDataData());
 			database.commit();
+			logger.info(data.getDataId().getDataId() + " saved");
 			return true;
 		}
 	}
 
 	@Override
 	public List<DataId> findAll() {
+		logger.info("Find all ids");
 		List<DataId> dataIds = new ArrayList<>();
 		for (DataId dataId: databaseMap.keySet()) {
 			dataIds.add(dataId);
 		}
+		logger.info("Found " + dataIds.size());
 		return dataIds;
 	}
 
 	@Override
 	public Optional<Data> findById(DataId dataId) {
+		logger.info("Find " + dataId);
 		if (databaseMap.containsKey(dataId)) {
+			logger.info(dataId.getDataId() + " found");
 			return Optional.of(new Data(dataId, databaseMap.get(dataId)));
 		} else {
+			logger.info(dataId.getDataId() + " not found");
 			return Optional.empty();
 		}
 	}
 
 	@Override
 	public boolean delete(DataId dataId) {
+		logger.info("Delete " + dataId.getDataId());
 		if (databaseMap.containsKey(dataId)) {
 			databaseMap.remove(dataId);
 			database.delete(dataId.getDataId());
+			logger.info(dataId.getDataId() + " removed");
 			return true;
 		} else {
+			logger.info(dataId.getDataId() + " not found");
 			return false;
 		}
 	}
@@ -72,6 +87,7 @@ public class DatabaseService implements DatabaseApi {
 		database.commit();
 		database.compact();
 		database.close();
+		logger.info("Database closed");
 		return database.isClosed();
 	}
 }
