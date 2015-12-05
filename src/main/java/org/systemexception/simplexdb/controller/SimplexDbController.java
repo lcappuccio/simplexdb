0e9a91388c6291a4ec926c9a817bc54bdc37a94d
Leonardo Cappuccio <leo@systemexception.org>
2015-12-05 18:57:14 +0100

    wip rest api

--- src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
+++ src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
@@ -4,18 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.systemexception.simplexdb.database.DatabaseService;
 import org.systemexception.simplexdb.domain.Data;
 import org.systemexception.simplexdb.domain.DataId;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 /**
@@ -34,22 +32,20 @@ public class SimplexDbController {
 		this.databaseService = databaseService;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	HttpStatus save(final File dataFile) throws IOException {
-		DataId dataId = new DataId(dataFile.getName());
-		Path dataPath = dataFile.toPath();
-		Data data = new Data(dataId, Files.readAllBytes(dataPath));
-		logger.info("Save " + dataId);
+	@RequestMapping(method = RequestMethod.POST)
+	HttpStatus save(@RequestParam("file") MultipartFile dataFile) throws IOException {
+		DataId dataId = new DataId(dataFile.getOriginalFilename());
+		Data data = new Data(dataId, dataFile.getBytes());
+		logger.info("Save " + dataId.getDataId());
 		boolean saved = databaseService.save(data);
 		if (saved) {
 			return HttpStatus.CREATED;
 		} else {
-			return HttpStatus.BAD_REQUEST;
+			return HttpStatus.CONFLICT;
 		}
 	}
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.GET)
 	List<DataId> findAll() {
 		logger.info("Find all ids");
 		return databaseService.findAll();
