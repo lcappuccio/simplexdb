f2203e73ba8ab0f1abf1e20ef0eef75f051b60de
Leonardo Cappuccio <leo@systemexception.org>
2015-12-07 19:24:46 +0100

    fix sh script

--- insert_files.sh
+++ insert_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-FILES=/home/lcappuccio/DataSets/dataset_insert_CON_2015/tallysheets/*
+FILES=PATH_TO_FILES/*
 for f in $FILES
 do
 	echo "Insert file $f" 
-	curl -F "file="$f http://localhost:8080/simplexdb/save 
+	curl -F "file="@$f http://localhost:8080/simplexdb/save 
 done
