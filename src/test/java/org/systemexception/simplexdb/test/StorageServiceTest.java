ac48b5836fc5826b5b3ec68a121cc60536c13c67
Leonardo Cappuccio <leo@systemexception.org>
2015-12-08 23:07:03 +0100

    improve test

--- src/test/java/org/systemexception/simplexdb/test/StorageServiceTest.java
+++ src/test/java/org/systemexception/simplexdb/test/StorageServiceTest.java
@@ -1,6 +1,7 @@
 package org.systemexception.simplexdb.test;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.systemexception.simplexdb.domain.Data;
 import org.systemexception.simplexdb.domain.DataId;
@@ -20,6 +21,19 @@ public class StorageServiceTest {
 	private final static String STORAGE_FOLDER = "target" + File.separator + "test_output";
 	private final Data testData = new Data(new DataId("TEST"), "TEST".getBytes());
 	StorageService sut;
+
+	@BeforeClass
+	public static void setSut() {
+		File toRemove = new File(STORAGE_FOLDER);
+		if (toRemove.exists()) {
+			String[] files = toRemove.list();
+			for (String file: files) {
+				new File(STORAGE_FOLDER + File.separator + file).delete();
+			}
+		}
+		toRemove.delete();
+		assert(!toRemove.exists());
+	}
 
 	@Before
 	public void setUp() throws IOException {
