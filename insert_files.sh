6102342decce213643d18068d15e76358daa310b
Leonardo Cappuccio <leo@systemexception.org>
2015-12-12 17:17:49 +0100

    variable usage in sh script

--- insert_files.sh
+++ insert_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILES=PATH_TO_FILES/*
-for f in $FILES
+for f in ${FILES}
 do
 	echo "Insert file $f" 
-	curl -F "file="@$f http://localhost:8080/simplexdb/save 
+	curl -F "file="@${f} http://localhost:8080/simplexdb/save
 done
