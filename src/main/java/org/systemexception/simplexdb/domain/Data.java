ad33163c19f6a89c6a45aacf7b646b2af62e80cf
Leonardo Cappuccio <leo@systemexception.org>
2015-12-22 22:18:30 +0100

    add internal data id

--- src/main/java/org/systemexception/simplexdb/domain/Data.java
+++ src/main/java/org/systemexception/simplexdb/domain/Data.java
@@ -1,21 +1,31 @@
 package org.systemexception.simplexdb.domain;
 
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.UUID;
 
 /**
  * @author leo
  * @date 05/12/15 00:58
  */
-public class Data {
+public class Data implements Serializable{
 
+	private final String dataInternalId;
 	private final DataId dataId;
 	private final byte[] dataData;
 
+
 	public Data(final DataId dataId, final byte[] dataData) {
+		this.dataInternalId = UUID.randomUUID().toString();
+
 		this.dataId = dataId;
 		this.dataData = dataData;
 	}
 
+	public String getDataInternalId() {
+		return dataInternalId;
+	}
+	
 	public DataId getDataId() {
 		return dataId;
 	}
@@ -31,6 +41,8 @@ public class Data {
 
 		Data data = (Data) o;
 
+		if (dataInternalId != null ? !dataInternalId.equals(data.dataInternalId) : data.dataInternalId != null)
+			return false;
 		if (dataId != null ? !dataId.equals(data.dataId) : data.dataId != null) return false;
 		return Arrays.equals(dataData, data.dataData);
 
@@ -38,7 +50,8 @@ public class Data {
 
 	@Override
 	public int hashCode() {
-		int result = dataId != null ? dataId.hashCode() : 0;
+		int result = dataInternalId != null ? dataInternalId.hashCode() : 0;
+		result = 31 * result + (dataId != null ? dataId.hashCode() : 0);
 		result = 31 * result + Arrays.hashCode(dataData);
 		return result;
 	}
