33f26a5f21fd534be60c3855027102a24b8ca094
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-12-23 12:56:49 +0100

    add launch/debug/profile shell script

---
+++ execute-app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# DEBUG
+# -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
+
+# Profile with Flight Recorder
+# -ea -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=name=test_results.jfr -XX:FlightRecorderOptions=settings=max-settings.jfc,defaultrecording=true,dumponexit=true,dumponexitpath=test_results.jfr
+
+java -jar simplexdb-0.8-SNAPSHOT.jar
