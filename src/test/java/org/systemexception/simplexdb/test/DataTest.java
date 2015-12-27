6764ae61cb5c1fd6a01c1d0d9e79aaf6a9887b0b
Leonardo Cappuccio <leo@systemexception.org>
2015-12-27 19:38:31 +0100

    improve test coverage

---
+++ src/test/java/org/systemexception/simplexdb/test/DataTest.java
@@ -0,0 +1,42 @@
+package org.systemexception.simplexdb.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.systemexception.simplexdb.domain.Data;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author leo
+ * @date 27/12/15 19:14
+ */
+public class DataTest {
+
+	private Data sut;
+	private byte[] bytes = new byte[256];
+	private String dataName = "TEST_DATA";
+
+	@Before
+	public void setUp() {
+		sut = new Data(dataName, bytes);
+	}
+
+	@Test
+	public void data_size_is_correct() {
+		assertTrue("256 Byte".equals(sut.getDataSize()));
+	}
+
+	@Test
+	public void data_size_Kb() {
+		byte[] bytes = new byte[2048];
+		sut = new Data(dataName, bytes);
+		assertTrue("2.0 KB".equals(sut.getDataSize()));
+	}
+
+	@Test
+	public void data_size_Mb() {
+		byte[] bytes = new byte[2048 * 1024];
+		sut = new Data(dataName, bytes);
+		assertTrue("2.0 MB".equals(sut.getDataSize()));
+	}
+}
