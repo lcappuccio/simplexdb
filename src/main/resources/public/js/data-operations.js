9edac3afbb5141acbd7836508971d02a60074c66
Leonardo Cappuccio <leo@systemexception.org>
2016-03-06 02:36:25 +0100

    Strings must use doublequote.

--- src/main/resources/public/js/data-operations.js
+++ src/main/resources/public/js/data-operations.js
@@ -1,46 +1,46 @@
 function uploadProgress(evt) {
 	if (evt.lengthComputable) {
 		var percentComplete = Math.round(evt.loaded * 100 / evt.total);
-		document.getElementById('uploadBtn').value = 'Progress: ' + percentComplete.toString() + '%';
+		document.getElementById("uploadBtn").value = "Progress: " + percentComplete.toString() + "%";
 	}
 	else {
-		document.getElementById('uploadBtn').value = 'Progress: Unable to compute';
+		document.getElementById("uploadBtn").value = "Progress: Unable to compute";
 	}
 }
 
 function uploadComplete(evt) {
-	document.getElementById('uploadBtn').value = 'Upload';
+	document.getElementById("uploadBtn").value = "Upload";
 }
 
 function uploadFailed(evt) {
-	document.getElementById('uploadBtn').value = 'Failed';
+	document.getElementById("uploadBtn").value = "Failed";
 }
 
 function uploadCanceled(evt) {
-	document.getElementById('uploadBtn').value = 'Canceled';
+	document.getElementById("uploadBtn").value = "Canceled";
 }
 
 function fileSizeCalculator(fileSize) {
 	var fileSizeString = "";
 	if (fileSize > 1000 * 1000) {
-		fileSizeString = (Math.round(fileSize * 100 / (1000 * 1000)) / 100).toString() + 'MB';
+		fileSizeString = (Math.round(fileSize * 100 / (1000 * 1000)) / 100).toString() + "MB";
 	}
 	else {
-		fileSizeString = (Math.round(fileSize * 100 / 1000) / 100).toString() + 'KB';
+		fileSizeString = (Math.round(fileSize * 100 / 1000) / 100).toString() + "KB";
 	}
 	return fileSizeString;
 }
 
 function saveData(data) {
 	$.ajax({
-		url: '/simplexdb/findbyid/' + data,
-		type: 'GET',
+		url: "/simplexdb/findbyid/" + data,
+		type: "GET",
 		statusCode: {
 			302: function (response) {
-				document.getElementById('saveBtn_' + data).style.backgroundColor = 'green';
+				document.getElementById("saveBtn_" + data).style.backgroundColor = "green";
 			},
 			404: function (response) {
-				document.getElementById('saveBtn_' + data).style.backgroundColor = 'red';
+				document.getElementById("saveBtn_" + data).style.backgroundColor = "red";
 			}
 		}
 	});
@@ -48,19 +48,19 @@ function saveData(data) {
 
 function deleteData(data) {
 	$.ajax({
-		url: '/simplexdb/delete/' + data,
-		type: 'DELETE',
+		url: "/simplexdb/delete/" + data,
+		type: "DELETE",
 		success: function (response) {
-			document.getElementById('saveBtn_' + data).disabled = true;
-			document.getElementById('deleteBtn_' + data).disabled = true;
+			document.getElementById("saveBtn_" + data).disabled = true;
+			document.getElementById("deleteBtn_" + data).disabled = true;
 		}
 	});
 }
 
 function exportData() {
 	$.ajax({
-		url: '/simplexdb/export/',
-		type: 'GET',
+		url: "/simplexdb/export/",
+		type: "GET",
 		success: function (response) {
 			console.log(response);
 		}
@@ -68,32 +68,32 @@ function exportData() {
 }
 
 function fileSelected() {
-	var files = document.getElementById('fileUploadBtn').files;
+	var files = document.getElementById("fileUploadBtn").files;
 	if (files.length > 1) {
 		var filesCount = files.length;
-		document.getElementById('fileName').innerHTML = 'File count: ' + files.length;
-		document.getElementById('fileType').innerHTML = 'Type: multiple';
+		document.getElementById("fileName").innerHTML = "File count: " + files.length;
+		document.getElementById("fileType").innerHTML = "Type: multiple";
 		var fileSize = 0;
 		for (i = 0; i < files.length; i++) {
 			fileSize += files[i].size;
 		}
 		var fileSizeTotal = fileSizeCalculator(fileSize);
 
-		document.getElementById('fileSize').innerHTML = 'Size: ' + fileSizeTotal;
+		document.getElementById("fileSize").innerHTML = "Size: " + fileSizeTotal;
 	} else {
-		var file = document.getElementById('fileUploadBtn').files[0];
+		var file = document.getElementById("fileUploadBtn").files[0];
 		if (file) {
 			var fileSizeTotal = fileSizeCalculator(file.size);
 
-			document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
-			document.getElementById('fileSize').innerHTML = 'Size: ' + fileSizeTotal;
-			document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
+			document.getElementById("fileName").innerHTML = "Name: " + file.name;
+			document.getElementById("fileSize").innerHTML = "Size: " + fileSizeTotal;
+			document.getElementById("fileType").innerHTML = "Type: " + file.type;
 		}
 	}
 }
 
 function uploadFile() {
-	var filesToUpload = document.getElementById('fileUploadBtn').files;
+	var filesToUpload = document.getElementById("fileUploadBtn").files;
 	for (i = 0; i < filesToUpload.length; i++) {
 		var formData = new FormData();
 		formData.append("fileToUpload", filesToUpload[i]);
@@ -108,5 +108,5 @@ function uploadFile() {
 }
 
 $(document).ready(function () {
-	$('#dataTable').DataTable();
+	$("#dataTable").DataTable();
 });
