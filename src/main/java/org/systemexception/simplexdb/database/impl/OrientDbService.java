4f9342444c1d493273a9732ef0196646df63d81e
Leonardo Cappuccio <cappuccio@strands.com>
2016-03-15 10:09:34 +0100

    wip

--- src/main/java/org/systemexception/simplexdb/database/impl/OrientDbService.java
+++ src/main/java/org/systemexception/simplexdb/database/impl/OrientDbService.java
@@ -1,8 +1,24 @@
 package org.systemexception.simplexdb.database.impl;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import org.systemexception.simplexdb.constants.LogMessages;
+import org.systemexception.simplexdb.database.AbstractDbService;
+import org.systemexception.simplexdb.service.StorageServiceApi;
+
+import java.io.File;
+
 /**
  * @author cappuccio
  * @date 15/03/16 09:59
  */
-public class OrientDbService {
+public class OrientDbService extends AbstractDbService {
+
+	public OrientDbService(final StorageServiceApi storageService, final String databaseName,
+	                       final Long maxMemoryOccupation) {
+
+		this.databaseName = databaseName;
+		logger.info(LogMessages.CREATE_DATABASE + databaseName);
+		String dbPath = System.getProperty("user.dir") + File.separator + databaseName;
+		ODatabaseDocumentTx database = new ODatabaseDocumentTx("plocal:" + dbPath).create();
+	}
 }
---
+++ src/test/java/org/systemexception/simplexdb/test/database/OrientDbServiceTest.java
@@ -0,0 +1,9 @@
+package org.systemexception.simplexdb.test.database;
+
+/**
+ * @author cappuccio
+ * @date 15/03/16 10:08
+ */
+public abstract class OrientDbServiceTest extends AbstractDbTest {
+
+}
