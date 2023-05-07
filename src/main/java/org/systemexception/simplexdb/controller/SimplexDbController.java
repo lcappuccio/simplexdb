2a7ab6478944f9e77ec11d7d12db915700a4ab8d
Leonardo <lcappuccio@some-random-email.org>
2023-05-07 15:43:39 +0200

    some detail about controller required parameters

--- src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
+++ src/main/java/org/systemexception/simplexdb/controller/SimplexDbController.java
@@ -64,7 +64,7 @@ public class SimplexDbController {
 	@RequestMapping(value = {Endpoints.FIND, Endpoints.FIND + Endpoints.ID_WITH_EXTENSION},
 			method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public ResponseEntity<List<Data>> find(@PathVariable(value = "id", required = false) final Optional<String> id)
+	public ResponseEntity<List<Data>> find(@PathVariable(value = "id") final Optional<String> id)
 			throws IOException, ClassNotFoundException {
 
 		List<Data> dataList = new ArrayList<>();
